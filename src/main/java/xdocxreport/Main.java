da15fb0f392c1e3ab9a2958b122eba57a09cb595
Christophe Dufour <christophe@dooapp.com>
2014-02-28 11:17:18 +0100

    Add a new test

--- src/main/java/xdocxreport/Main.java
+++ src/main/java/xdocxreport/Main.java
@@ -3,18 +3,15 @@ package xdocxreport;
 import fr.opensagres.xdocreport.document.IXDocReport;
 import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
 import fr.opensagres.xdocreport.template.IContext;
-import fr.opensagres.xdocreport.template.ITemplateEngine;
 import fr.opensagres.xdocreport.template.TemplateEngineKind;
-import fr.opensagres.xdocreport.template.formatter.FieldMetadata;
 import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
-import fr.opensagres.xdocreport.template.registry.TemplateEngineRegistry;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.DecimalFormat;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by christophedufour on 17/02/2014.
@@ -28,17 +25,19 @@ public class Main {
 
         // 2) Create Java model context
         FieldsMetadata fieldsMetadata = report.createFieldsMetadata();
-        fieldsMetadata.addFieldAsList("ashortvalue");
-        fieldsMetadata.addFieldAsList("alonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglongvalue");
-
+        //fieldsMetadata.addFieldAsList("ashortvalue");
+        fieldsMetadata.addFieldAsList("a");
 
 
         report.setFieldsMetadata(fieldsMetadata);
         IContext context = report.createContext();
         context.put("name", "world");
+
+        context.put("number", new DecimalFormat("#.00000"));
+        context.put("n", new DecimalFormat("#.00000"));
         context.put("averyveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryverylongname", "value");
         context.put("ashortvalue", Arrays.asList(new String[]{"zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf"}));
-        context.put("alonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglongvalue", Arrays.asList(new String[]{"zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf", "zefezf"}));
+        context.put("a", Arrays.asList(new Tmp[]{new Tmp(),new Tmp(),new Tmp(),new Tmp(),new Tmp(),new Tmp()}));
 
 
         // 3) Generate report by merging Java model with the ODT
@@ -46,5 +45,18 @@ public class Main {
         report.process(context, out);
     }
 
+
+    public static class Tmp {
+        public double value=Math.random();
+
+
+        public double getValue() {
+            return value;
+        }
+
+        public void setValue(double value) {
+            this.value = value;
+        }
+    }
 
 }
